9dbd5ffdca0cbc25117e237d1d015424b70ce94b
Luke Chatelain <jens.chatelain@gmail.com>
2021-05-20 16:56:34 +0900

    made a movies app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # A user can see all the lists
+  resources :lists do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only:[:destroy]
+
 end
+
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+require 'JSON'
+require "rest-client"
+
+puts "...clearing database"
+
+Movie.destroy_all
+
+puts "...Creating movies"
+
+response = RestClient.get "http://tmdb.lewagon.com/movie/top_rated"
+repos = JSON.parse(response)
+  array = repos["results"]
+  array.each do |obj|
+    Movie.create(
+      overview: obj["overview"],
+      title: obj["title"],
+      overview: obj["overview"],
+      poster_url: ("https://image.tmdb.org/t/p/w500" + obj["poster_path"]),
+      rating: obj["vote_average"].to_f
+      )
+  end
+puts "created #{Movie.count} movies"
+
